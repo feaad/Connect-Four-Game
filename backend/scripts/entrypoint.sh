8b064e54787c5515c8cbb072d346a83d0662e97f
feaad <antwi-donkor_f@outlook.com>
2024-07-23 12:33:51 +0200

    Loading sample data

--- backend/scripts/entrypoint.sh
+++ backend/scripts/entrypoint.sh
@@ -6,6 +6,13 @@ python manage.py wait_for_db
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 
+if [[ ! -z "${LOAD_SAMPLE_DATA}" ]]; then
+	if [[ "$LOAD_SAMPLE_DATA" == "True" ]]; then
+		python manage.py flush --noinput
+		python manage.py loaddata sample_data.json
+	fi
+fi
+
 if [[ ! -z "${SUPERUSER_USERNAME}" ]]; then
 	python manage.py init_su
 fi
