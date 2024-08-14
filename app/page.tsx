6d82e4290e117434871ea32750ffb19664b917d9
feaad <antwi-donkor_f@outlook.com>
2024-08-14 17:05:37 +0100

    global left side of layout implemented

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,24 @@
+import React from "react";
+import Image from 'next/image';
+import connect_four from '../public/images/Connect_four_logo.png';
+
 export default function Home() {
   return (
     <main>
-      <h1>Hello world</h1>
+      <div className="grid grid-cols-2 gap-4">
+        <div className=" gridLeft font-sans text-5xl text-balance leading-normal">
+          <h1>The <br /> Connect Four <br />Game</h1>
+          <br/>
+          <Image src={connect_four} alt="Connect Four" width={400} height={500} />
+        </div>
+        
+        <div>
+          <h1>Hello</h1>
+        </div>
+        
+          
+      </div>
+      
     </main>
   )
 }
