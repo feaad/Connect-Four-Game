d0b49f22aee991a2dec96e0bb9a627b468e829b8
feaad <antwi-donkor_f@outlook.com>
2024-08-15 17:18:07 +0100

    Adjusting button and font sizes

--- components/Buttons.tsx
+++ components/Buttons.tsx
@@ -1,23 +1,25 @@
-import React from 'react'
+import React from "react";
 
 const Buttons = () => {
-    const buttons = [
-        { label: 'Play online with other players', link: '/play-online' },
-        { label: 'Send a link to play with a friend', link: '/send-link' },
-        { label: 'Play with our AI', link: '/play-ai' },
-    ]
+  const buttons = [
+    { label: "Play online with other players", link: "/play-online" },
+    { label: "Send a link to play with a friend", link: "/send-link" },
+    { label: "Play with our AI", link: "/play-ai" },
+  ];
   return (
+    <div>
       <div>
-          
-          <div>
-              {buttons.map((button, index) => (
-                  <button key={index} className="bg-btn-colour btn btn-block mb-6 justify-start rounded font-normal text-white">
-                      {button.label}
-                  </button>
-              ))}
-          </div>
+        {buttons.map((button, index) => (
+          <button
+            key={index}
+            className="bg-btn-colour btn btn-block mb-6 h-20 justify-start rounded font-normal text-white text-base"
+          >
+            {button.label}
+          </button>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Buttons
+export default Buttons;
