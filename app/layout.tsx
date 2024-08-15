189d94c9b54730779378c89d194c59cb8e0d9df3
feaad <antwi-donkor_f@outlook.com>
2024-08-15 10:15:55 +0100

    Set up Radix UI

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
+import '@radix-ui/themes/styles.css';
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { Theme } from '@radix-ui/themes';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,11 +18,15 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      
       <body className={inter.className}>
+        <Theme>
         <main>
           {children}
-        </main>
+          </main>
+          </Theme>
       </body>
+      
     </html>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,29 @@
 import React from "react";
 import Image from 'next/image';
 import connect_four from '../public/images/Connect_four_logo.png';
+import { Button } from '@radix-ui/themes';
 
 export default function Home() {
   return (
     <main>
-      <div className="grid grid-cols-2 gap-4">
+      <div className="grid grid-cols-2 gap-8">
         <div className=" gridLeft font-sans text-5xl text-balance leading-normal">
           <h1>The <br /> Connect Four <br />Game</h1>
           <br/>
           <Image src={connect_four} alt="Connect Four" width={400} height={500} />
         </div>
         
-        <div>
-          <h1>Hello</h1>
+        <div className="gridRight ">
+          <h1 className="text-2xl font-medium leading-loose">Get Started</h1>
+          <p className="font-light">Play the Connect Four Game online with players around the
+            world, send an invitation link to a friend or play a game with the different types
+            of AI!</p>
+          <br />
+          <div>
+            <Button>Play Now</Button>
+          </div>
         </div>
+
         
           
       </div>
