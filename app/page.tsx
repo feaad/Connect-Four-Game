e2167a855fb3e0e1870e278ec5517014fa206b6a
feaad <antwi-donkor_f@outlook.com>
2024-08-15 10:50:44 +0100

    Set up of DaisyUI

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Image from 'next/image';
-import connect_four from '../public/images/Connect_four_logo.png';
-import { Button } from '@radix-ui/themes';
+import cfLogo from "@/assets/images/connect_four_logo.svg"
+
 
 export default function Home() {
   return (
@@ -10,7 +10,7 @@ export default function Home() {
         <div className=" gridLeft font-sans text-5xl text-balance leading-normal">
           <h1>The <br /> Connect Four <br />Game</h1>
           <br/>
-          <Image src={connect_four} alt="Connect Four" width={400} height={500} />
+          <Image src={cfLogo} alt="Connect Four" width={400} height={500} />
         </div>
         
         <div className="gridRight ">
@@ -20,13 +20,15 @@ export default function Home() {
             of AI!</p>
           <br />
           <div>
-            <Button>Play Now</Button>
+            <button className="btn btn-block">Play Now</button>
           </div>
         </div>
 
         
           
       </div>
+
+      
       
     </main>
   )
