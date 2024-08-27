471cae20ba3627ae42570fd71c0737397c02aeb0
feaad <antwi-donkor_f@outlook.com>
2024-08-27 15:48:56 +0100

    Menu button interactivity

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,22 +1,28 @@
 "use client";
 import { logout } from "@/actions/logout";
-import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { faBars, faXmark } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 
 const NavBar = () => {
+  const [buttonChange, setButtonChange] = useState(false);
+
   return (
     <div>
       <div className="absolute right-0 top-0 m-auto flex flex-row justify-end">
         <details className="dropdown dropdown-end">
-          <summary className="btn m-1 border-none bg-white shadow-none">
-            <FontAwesomeIcon className="h-5 w-5" icon={faBars} />
-          </summary>
-          {/* <ul tabIndex={0} className="p2 bg-base-00 menu dropdown-content w-32 rounded-box shadow"> */}
-          <ul
-            className="menu dropdown-content z-[1] w-52 rounded-box bg-base-100 p-2 shadow"
+          <summary
+            className="btn m-1 border-none bg-white shadow-none"
+            onMouseDown={() => setButtonChange(!buttonChange)}
           >
+            {buttonChange ? (
+              <FontAwesomeIcon className="h-5 w-5" icon={faXmark} />
+            ) : (
+              <FontAwesomeIcon className="h-5 w-5" icon={faBars} />
+            )}
+          </summary>
+          <ul className="menu dropdown-content z-[1] w-52 rounded-box bg-base-100 p-2 shadow">
             <li>
               <Link href="/">Home</Link>
               <Link href="/">Profile</Link>
