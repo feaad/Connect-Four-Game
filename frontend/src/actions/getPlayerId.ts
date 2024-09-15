3b16635472ac88fcad69fddac6bdf9696a428bfa
feaad <antwi-donkor_f@outlook.com>
2024-09-15 16:59:12 +0100

    Clearing the frontend folder

--- frontend/src/actions/getPlayerId.ts
+++
@@ -1,9 +0,0 @@
-"use server";
-
-import { auth } from "@/auth";
-
-export const getPlayerId = async () => {
-  const session = await auth();
-
-  return session?.player_id;
-};
--- frontend/src/actions/getWSUrl.ts
+++
@@ -1,7 +0,0 @@
-"use server";
-
-import { env } from "@/lib/env";
-
-export const getWSUrl = async () => {
-  return env.WS_URL;
-};
